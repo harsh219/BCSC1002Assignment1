7108069ba407dc435b3135f9e1b1441a8684cf54
Harsh Agarwal <harsh.agarwal_cs19@gla.ac.in>
2020-08-30 23:33:29 +0530

    initial commit

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -6,42 +6,6 @@
  * */
 package definitions;
 
-import java.util.Arrays;
-
 public class Library {
-    private Book[] currentlyAvailableBooks;
 
-    public Library(Book[] currentlyAvailableBooks) {
-        this.currentlyAvailableBooks = currentlyAvailableBooks;
-    }
-
-    public Library() {
-        this.currentlyAvailableBooks = getCurrentlyAvailableBooks();
-    }
-
-    public Book[] getCurrentlyAvailableBooks() {
-        return currentlyAvailableBooks;
-    }
-
-    public void setCurrentlyAvailableBooks(Book[] currentlyAvailableBooks) {
-        this.currentlyAvailableBooks = currentlyAvailableBooks;
-    }
-
-    @Override
-    public String toString() {
-        return Arrays.toString(currentlyAvailableBooks);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Library library = (Library) o;
-        return Arrays.equals(getCurrentlyAvailableBooks(), library.getCurrentlyAvailableBooks());
-    }
-
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(getCurrentlyAvailableBooks());
-    }
 }
