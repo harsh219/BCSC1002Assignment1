6b03d90c765d8ee1ec75d925a240a92e4bb956e5
Harsh Agarwal <harsh.agarwal_cs19@gla.ac.in>
2020-08-30 23:11:15 +0530

    import package Library in FrontDesk.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,10 +1,47 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
+ *  User: Harsh Agarwal
  *  Date: 21/08/20
  *  Time: 3:50 PM
  *  File Name : Library.java
  * */
 package definitions;
 
+import java.util.Arrays;
+
 public class Library {
+    private Book[] currentlyAvailableBooks;
+
+    public Library(Book[] currentlyAvailableBooks) {
+        this.currentlyAvailableBooks = currentlyAvailableBooks;
+    }
+
+    public Library() {
+        this.currentlyAvailableBooks = getCurrentlyAvailableBooks();
+    }
+
+    public Book[] getCurrentlyAvailableBooks() {
+        return currentlyAvailableBooks;
+    }
+
+    public void setCurrentlyAvailableBooks(Book[] currentlyAvailableBooks) {
+        this.currentlyAvailableBooks = currentlyAvailableBooks;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(currentlyAvailableBooks);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Arrays.equals(getCurrentlyAvailableBooks(), library.getCurrentlyAvailableBooks());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getCurrentlyAvailableBooks());
+    }
 }
