a211051b9f50923fc71297b8dbfb6a7ce1dc993d
Harsh Agarwal <harsh.agarwal_cs19@gla.ac.in>
2020-08-30 23:37:09 +0530

    final commit

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,6 +6,60 @@
  * */
 package definitions;
 
-public class Book {
+import java.util.Objects;
 
+public class Book {
+    private String bookName;
+    private String authorName;
+    private String ISBNNumber;
+
+    public Book(String bookName, String authorName, String ISBNNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.ISBNNumber = ISBNNumber;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getISBNNumber() {
+        return ISBNNumber;
+    }
+
+    public void setISBNNumber(String ISBNNumber) {
+        this.ISBNNumber = ISBNNumber;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Book Name: %s, Author Name: %s, ISBN Number: %s", getBookName(), getAuthorName(), getISBNNumber());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(getBookName(), book.getBookName()) &&
+                Objects.equals(getAuthorName(), book.getAuthorName()) &&
+                Objects.equals(getISBNNumber(), book.getISBNNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBookName(), getAuthorName(), getISBNNumber());
+    }
 }
