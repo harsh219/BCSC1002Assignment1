87a06d12ffecc107900be1efcd455c134cc1980e
Harsh Agarwal <harsh.agarwal_cs19@gla.ac.in>
2020-08-30 23:08:00 +0530

    creating fields in FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -12,5 +12,11 @@ import java.util.Scanner;
 public class FrontDesk {
     public static void main(String[] args) {
         Scanner object = new Scanner(System.in);
+        System.out.println("Enter your details : First name, Middle name, Last name, University Roll Number, Number Of Books Issued : ");
+        String studentFirstName = object.nextLine();
+        String studentMiddleName = object.nextLine();
+        String studentLastName = object.nextLine();
+        long studentUniversityRollNumber = object.nextLong();
+        int totalBooksIssuedByStudent = object.nextInt();
     }
 }
