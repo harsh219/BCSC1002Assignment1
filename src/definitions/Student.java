6b548936bc8f094407a503a4bd50817341adc162
Harsh Agarwal <harsh.agarwal_cs19@gla.ac.in>
2020-08-30 23:32:30 +0530

    final commit

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -6,6 +6,113 @@
  * */
 package definitions;
 
-public class Student {
+import java.util.Arrays;
+import java.util.Objects;
 
+public class Student {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private long universityRollNumber;
+    private int numberOfBooksIssued;
+    private Book[] namesOfTheBooksIssued;
+
+    public Student(String firstName, String middleName, String lastName, long universityRollNumber, int numberOfBooksIssued) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.universityRollNumber = universityRollNumber;
+        this.numberOfBooksIssued = numberOfBooksIssued;
+        this.namesOfTheBooksIssued = new Book[numberOfBooksIssued];
+        for (int index = 0; index < namesOfTheBooksIssued.length; index++) {
+            namesOfTheBooksIssued[index] = new Book("Book" + (index + 1), "Author" + (index + 1), "1234567891234");
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getUniversityRollNumber() {
+        return universityRollNumber;
+    }
+
+    public void setUniversityRollNumber(long universityRollNumber) {
+        this.universityRollNumber = universityRollNumber;
+    }
+
+    public int getNumberOfBooksIssued() {
+        return numberOfBooksIssued;
+    }
+
+    public void setNumberOfBooksIssued(int numberOfBooksIssued) {
+        this.numberOfBooksIssued = numberOfBooksIssued;
+    }
+
+    public Book[] getNamesOfTheBooksIssued() {
+        return namesOfTheBooksIssued.clone();
+    }
+
+    public void setNamesOfTheBooksIssued(Book[] namesOfTheBooksIssued) {
+        this.namesOfTheBooksIssued = namesOfTheBooksIssued;
+    }
+
+    public void printNamesOfBooksIssued() {
+        for (Book bookName : namesOfTheBooksIssued) {
+            System.out.println(bookName);
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", universityRollNumber=" + universityRollNumber +
+                ", numberOfBooksIssued=" + numberOfBooksIssued +
+                ", namesOfTheBooksIssued=" + Arrays.toString(namesOfTheBooksIssued) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return getUniversityRollNumber() == student.getUniversityRollNumber() &&
+                getNumberOfBooksIssued() == student.getNumberOfBooksIssued() &&
+                Objects.equals(getFirstName(), student.getFirstName()) &&
+                Objects.equals(getMiddleName(), student.getMiddleName()) &&
+                Objects.equals(getLastName(), student.getLastName()) &&
+                Arrays.equals(getNamesOfTheBooksIssued(), student.getNamesOfTheBooksIssued());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(getFirstName(), getMiddleName(), getLastName(), getUniversityRollNumber(), getNumberOfBooksIssued());
+        result = 31 * result + Arrays.hashCode(getNamesOfTheBooksIssued());
+        return result;
+    }
 }
